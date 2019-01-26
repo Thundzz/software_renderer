e6428aafae0fd1d5b87b56af332ae1e4fdf937df
Thundzz <yacinenew@gmail.com>
2019-01-26 11:17:01 +0100

    simple basic sdl2 project in rust

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate sdl2; 
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+ 
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+ 
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+ 
+    let mut canvas = window.into_canvas().build().unwrap();
+ 
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut j : u16 = 0;
+    'running: loop {
+    	j = (j + 1) % 255;
+        let i : u8 = j as u8;
+        println!("{}", i);
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                }
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 100_000_000u32 / 60));
+    }
+}
