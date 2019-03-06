db8ddc53358822c12b0a27e64b2a52ce6e07332b
Yacine EL JARRARI <yacine.el-jarrari@atinternet.com>
2019-03-06 14:14:33 +0100

    starfield effect

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
  
-    let window: Window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window: Window = video_subsystem.window("rust-sdl2 demo", 1024, 1024)
         .position_centered()
         .build()
         .unwrap();
@@ -32,14 +32,17 @@ pub fn main() {
     let renderer = renderer::MyRenderer { };
 
     let mut starfield : star::StarField = star::StarField::new_star_field();
-    let mut bm : bitmap::BitMap = bitmap::BitMap::new(800, 600);
+    //let mut bm : bitmap::BitMap = bitmap::BitMap::new(800, 600);
 
     'running: loop {
 
+        let mut bm : bitmap::BitMap = bitmap::BitMap::new(1024, 1024);
 
-        // println!("{}", i);
+
+        // Scan events
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
+
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -53,12 +56,13 @@ pub fn main() {
 
         //canvas.set_draw_color(Color::RGB(255, 255, 255));
 
+        // Move stars
         for i in 0..starfield.stars.len() {
-            // let point = Point::new(i, i + j as i32);
-            //println!("{} {}", x, y);
             let star = starfield.stars[i];
-            starfield.stars[i] = star::Star { x : star.x, y : star.y + 0.001, z: star.z }
+            starfield.stars[i] = star::Star { x : star.x, y : star.y + 0.0015, z: star.z }
         }
+        //println!("{:?}", starfield.stars.len());
+
         renderer::MyRenderer::render(&starfield, &mut bm);
 
         let width = bm.width;
@@ -68,11 +72,16 @@ pub fn main() {
             for y in 0..height {
                 let color = bm.get(x, y);
                 canvas.set_draw_color(color);
-                let point = Point::new(x, y);
-                let res = canvas.draw_point(point);
+                if(color.r == 0 && color.g == 0 && color.b == 0){
+
+                } else {
+                    let point = Point::new(x, y);
+                    let res = canvas.draw_point(point);
+                }
             }
         }
         //println!("{} {} {}", color.r,  color.g, color.b);
+        //canvas.clear();
         canvas.present();
 
         ::std::thread::sleep(Duration::new(0, 16_000_000u32 / 60));
--- src/renderer.rs
+++ src/renderer.rs
@@ -7,8 +7,8 @@ pub struct MyRenderer;
 
 impl MyRenderer  {
     pub fn render<'a, 'b>(star_field: &'a StarField, bitmap : &'b mut BitMap) -> &'b BitMap {
-        let width = 800;
-        let height = 600;
+        let width = 1024;
+        let height = 1024;
 
         let white_pixel = sdl2::pixels::Color::RGB(255, 255,255);
 
@@ -17,7 +17,7 @@ impl MyRenderer  {
             let x = (star.x * width as f64).floor() as i32;
             let y = (star.y * height as f64).floor() as i32;
             //println!("{} {}", x, y);
-            bitmap.replace(x, y, white_pixel);
+            bitmap.replace(x, y % height, white_pixel);
         }
         return bitmap;
     }
